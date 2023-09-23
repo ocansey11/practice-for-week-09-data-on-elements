31a7cc61b2a19dd5442f0c1b8d9ed4885eb54c30
kevin ocansey <kevinocansey11@gmail.com>
2023-09-23 13:41:55 +0000

    90% complete, however the item i want to add to the shopping list appears only for a split second before disappearing

--- data.js
+++ data.js
@@ -1,1 +1,25 @@
 // Your code here
+// Pseudocode
+//click/select on an option
+//press add button
+//get target value
+//add/append target value to the ul's inner html
+//pass their data-attributes into css style and give each a different colour
+
+window.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("add");
+  const type = document.getElementById("type");
+  const shoppingList = document.getElementById("shopping-list");
+
+  function addItem() {
+    const selectCollection = type.selectedOptions;
+    const selectedType = selectCollection[0].innerText;
+
+    const shoppingLi = document.createElement("li");
+    shoppingLi.innerHTML = selectedType;
+    shoppingLi.setAttribute("data-type", selectCollection[0].value);
+    shoppingList.appendChild(shoppingLi);
+  }
+
+  button.addEventListener("click", addItem);
+});
